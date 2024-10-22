79f94f4b2c07286cce4168c29f20a7f1bca0d96c
Harsimran Preet Singh <153043163+04harsimran20@users.noreply.github.com>
2024-10-22 15:48:00 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -26,6 +26,11 @@ export default function Home() {
             Week 5 Assignment
           </Link>
         </li>
+        <li>
+          <Link href="/week-6" className="text-white hover:text-green-500 hover:underline">
+            Week 6 Assignment
+          </Link>
+        </li>
       </ul>
     </main>
   );
